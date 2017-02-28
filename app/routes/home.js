356cae8e213f091cb020bf2b93fac7f3c00aa404
Jeff <fenster.js@gmail.com>
2017-02-27 22:55:12 -0500

    Added a check for signed in user on home page

--- app/routes/home.js
+++ app/routes/home.js
@@ -5,35 +5,23 @@ export default Ember.Route.extend({
   store: service(),
   session: service(),
   model () {
-    let friends = this.get('session.data.authenticated.user.friends')
+    let friends = this.get('session.data.authenticated.user.friends');
     let promises = [];
 
-    for (var i = 0; i < friends.length; i++) {
-      var promise = this.get('store').query('blog-post', {
-        owner: friends[i]._id
-      })
-      promises.push(promise)
-      // .then(function(friendsPosts){
-      //   console.log("friendsPosts", friendsPosts)
-      //   posts.push(friendsPosts)
-      // });
+    if(friends){
+      for (var i = 0; i < friends.length; i++) {
+        var promise = this.get('store').query('blog-post', {
+          owner: friends[i]._id
+        });
+        promises.push(promise);
+      }
+      return Promise.all(promises)
+      .then(function(posts){
+        return posts;
+      });
+    } else {
+      return "SIGN UP"
     }
 
-    console.log(promises)
-
-    return Promise.all(promises)
-    .then(function(posts){
-      return posts
-    })
-
   }
-  // model: function (params) {
-
-  //   const posts =  this.get('store').query('blog-post', {
-  //       owner: params.user_id
-  //   }); // Makes a GET request to "http://localhost:1337/api/blog-posts?owner=5875a0ffff80ca1c5189f25c"
-
-  //   return posts;
-
-  // }
 });
