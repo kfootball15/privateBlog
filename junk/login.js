9ef7d0cdb9b0849e96623e308592b13405b5bf32
Jeff Fenster <jeff@Jeffs-iMac.local>
2017-03-06 21:25:07 -0500

    added SASS, began styling

--- junk/login.js
+++
@@ -1,6 +0,0 @@
-// routes/login.js
-
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-});
--- junk/user.js
+++
@@ -1,8 +0,0 @@
-//routes/user.js
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-  model() {
-    return this.store.findAll('user');
-  }
-});
